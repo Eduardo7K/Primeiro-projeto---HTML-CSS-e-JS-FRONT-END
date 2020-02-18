9ae12b019862307af13748fe35f032a36020ec0d
Eduardo Kuchenbecker <eduardo.k@lyncas.net>
2020-02-18 14:20:10 -0300

    codigo 18/02

--- log.js
+++ log.js
@@ -37,68 +37,6 @@ function newUser() {
   window.location.href = "form.html";
 }
 
-function deleteUser(i) {
-  if (confirm("Deseja realmente excluir?")) {
-    // var row = document.getElementById(i);
-    // row.parentNode.removeChild(row);
-
-    table1.splice(i, 1);
-    storageItems();
-
-    console.log(">>", Date.now() - 1580910400000);
-
-    setTimeout(() => window.location.reload(), 500);
-  }
-}
-
-var table1 = [
-  {
-    id: 1,
-    nome: "Michael Holz",
-    data: "04/10/2013",
-    role: "Admin",
-    status: "Active",
-    statusImg: "imgs\\green-circle.png",
-    img: "imgs\\michael.jpg",
-  },
-  {
-    id: 2,
-    nome: "Paula Wilson",
-    data: "05/08/2014",
-    role: "Publisher",
-    status: "Active",
-    statusImg: "imgs\\green-circle.png",
-    img: "imgs\\paula.jpg",
-  },
-  {
-    id: 3,
-    nome: "Antonio Moreno",
-    data: "11/05/2015",
-    role: "Publisher",
-    status: "Suspended",
-    statusImg: "imgs\\red-circle.png",
-    img: "imgs\\antonio.png",
-  },
-  {
-    id: 4,
-    nome: "Mary Saveley",
-    data: "06/09/2016",
-    role: "Reviewer",
-    status: "Active",
-    statusImg: "imgs\\green-circle.png",
-    img: "imgs\\mary.jpg",
-  },
-  {
-    id: 5,
-    nome: "Martin Sommer",
-    data: "12/08/2017",
-    role: "Moderator",
-    status: "Inactive",
-    statusImg: "imgs\\yellow-circle.png",
-    img: "imgs\\martin.jpg",
-  },
-];
-
 var table = document.createElement("table"),
   thead = document.createElement("thead"),
   tbody = document.createElement("tbody"),
@@ -140,85 +78,107 @@ function appendtable() {
   document.getElementById("tabela-dinamica").appendChild(table);
 }
 
-function updateTable(table1) {
-  for (var i = 0; i < table1.length; i++) {
-    tr = document.createElement("tr");
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+function getUsers() {
+  const url = "http://localhost:3003/api/users";
+  const http = new XMLHttpRequest();
+  http.open("GET", url);
+  http.send();
+  http.onreadystatechange = e => {
+    console.log(http.responseText);
+    console.log(typeof http.responseText);
+    let table1 = JSON.parse(http.responseText);
+    console.log(table1.length);
+    console.log(typeof table1);
 
-    //for #
-    td = document.createElement("td");
-    td.innerHTML = table1[i].id;
-    td.className = "idClass";
-    tr.appendChild(td);
+    console.log("teste");
+    console.log(table1);
+    console.log("teste");
 
-    //for name
-    var divImg = document.createElement("div");
-    td = document.createElement("td");
-    td.className = "nameClass";
-    var span1 = document.createElement("span");
-    span1.innerHTML = table1[i].nome;
-    td.appendChild(span1);
-    td.appendChild(divImg);
-    divImg.id = "div-name";
-    divImg.appendChild(span1);
+    updateTable(table1);
 
-    //for img
-    divImg.appendChild(document.createElement("img")).src = table1[i].img;
-    tr.appendChild(td);
+    function updateTable(table1) {
+      for (var i = 0; i < table1.length; i++) {
+        tr = document.createElement("tr");
 
-    //for date creation
-    td = document.createElement("td");
-    td.className = "dataClass";
-    td.innerHTML = table1[i].data;
-    tr.appendChild(td);
+        //for #
+        td = document.createElement("td");
+        td.innerHTML = table1[i].id;
+        td.className = "idClass";
+        tr.appendChild(td);
 
-    //for role
-    td = document.createElement("td");
-    td.className = "roleClass";
-    td.innerHTML = table1[i].role;
-    tr.appendChild(td);
+        //for name
+        var divImg = document.createElement("div");
+        td = document.createElement("td");
+        td.className = "nameClass";
+        var span1 = document.createElement("span");
+        span1.innerHTML = table1[i].pName;
+        td.appendChild(span1);
+        td.appendChild(divImg);
+        divImg.id = "div-name";
+        divImg.appendChild(span1);
 
-    //for status
-    td = document.createElement("td");
-    td.className = "statusClass";
-    td.innerHTML = table1[i].status;
-    tr.appendChild(td);
+        //for img
+        divImg.appendChild(document.createElement("img")).src = table1[i].img;
+        tr.appendChild(td);
 
-    //for status img
-    td.appendChild(document.createElement("img")).src = table1[i].statusImg;
-    td.id = "td-statusImg";
-    tr.appendChild(td);
+        //for date creation
+        td = document.createElement("td");
+        td.className = "dataClass";
+        td.innerHTML = table1[i].data;
+        tr.appendChild(td);
 
-    //for action
-    td = document.createElement("td");
-    td.className = "actionClass";
-    td.id = "td-action";
+        //for role
+        td = document.createElement("td");
+        td.className = "roleClass";
+        td.innerHTML = table1[i].role;
+        tr.appendChild(td);
 
-    //for gear image
-    var gear1 = document.createElement("img");
-    gear1.src = "imgs\\engine.png";
-    td.appendChild(gear1);
+        //for status
+        td = document.createElement("td");
+        td.className = "statusClass";
+        td.innerHTML = table1[i].status;
+        tr.appendChild(td);
 
-    //for delete image
-    var delImg = document.createElement("img");
-    delImg.src = "imgs\\x.png";
-    var tagA = document.createElement("a");
+        //for status img
+        td.appendChild(document.createElement("img")).src = table1[i].statusImg;
+        td.id = "td-statusImg";
+        tr.appendChild(td);
 
-    tagA.id = "a-tag";
-    let indexDel = i;
-    tagA.addEventListener("click", function() {
-      deleteUser([indexDel]);
-    });
+        //for action
+        td = document.createElement("td");
+        td.className = "actionClass";
+        td.id = "td-action";
 
-    tr.setAttribute("id", i);
+        //for gear image
+        var gear1 = document.createElement("img");
+        gear1.src = "imgs\\engine.png";
+        td.appendChild(gear1);
 
-    tagA.appendChild(delImg);
-    td.appendChild(tagA);
-    tr.appendChild(td);
+        //for delete image
+        var delImg = document.createElement("img");
+        delImg.src = "imgs\\x.png";
+        var tagA = document.createElement("a");
 
-    tbody.appendChild(tr);
-  }
+        tagA.id = "a-tag";
+        let indexDel = i;
+        tagA.addEventListener("click", function() {
+          deleteUser([indexDel]);
+        });
+
+        tr.setAttribute("id", i);
+
+        tagA.appendChild(delImg);
+        td.appendChild(tagA);
+        tr.appendChild(td);
+
+        tbody.appendChild(tr);
+      }
+    }
+  };
 }
 
+/*
 function checkLocalStorage() {
   if (localStorage.getItem("users") === null) {
     updateTable(table1);
@@ -269,13 +229,16 @@ function addNewItem() {
 
     table1.push({
       id: id,
-      nome: personName,
+      pName: personName,
       data: today,
       role: "Admin",
       status: "Active",
       statusImg: "imgs\\green-circle.png",
       img: personImg,
     });
+
+    
+
     storageItems();
     location.href = "http://localhost:5500/dashboard.html";
     //setTimeout(() => (location.href = "dashboard.html"), 500);
@@ -286,6 +249,7 @@ function addNewItem() {
     alert("Phone number must have 8 or 9 digits");
   }
 }
+*/
 
 function descBox() {
   var e = document.getElementById("question-image");
@@ -375,4 +339,64 @@ function keyMask() {
   const inputElement = document.getElementById("passportId");
   inputElement.addEventListener("keydown", enforceFormat);
   inputElement.addEventListener("keyup", formatToPhone);
+}
+
+/*function getUsers() {
+  const url = "http://localhost:3003/api/users";
+  const http = new XMLHttpRequest();
+  http.open("GET", url);
+  http.send();
+  http.onreadystatechange = e => {
+    console.log(http.responseText);
+  };
+}*/
+
+function deleteUser(i) {
+  if (confirm("Deseja realmente excluir?")) {
+    const url = `http://localhost:3003/api/users/${i}`;
+    console.log(i);
+    console.log(url);
+    const http = new XMLHttpRequest();
+    http.open("DELETE", url);
+    http.send();
+    http.onreadystatechange = e => {
+      console.log(http.responseText);
+    };
+
+    //setTimeout(() => window.location.reload(), 500);
+  }
+}
+
+async function postUser() {
+  const url = "http://localhost:3003/api/users";
+
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let personName = firstName + " " + lastName;
+  let personImg = document.getElementById("person-image").src;
+
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
+  today = dd + "/" + mm + "/" + yyyy;
+
+  let user = {
+    pName: personName,
+    data: today,
+    role: "Admin",
+    status: "Active",
+    statusImg: "imgs\\green-circle.png",
+    img: personImg,
+  };
+  console.log(user);
+
+  await fetch(url, {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
 }
